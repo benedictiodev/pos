9cd2ef64cdf7629c6e88791ff7e8344009602cde
yogaprianjaya <yogaprianjaya@gmail.com>
2024-04-29 22:33:22 +0700

    add controller cashflow

---
+++ app/Http/Controllers/CashFlowController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CashIn;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CashFlowController extends Controller
+{
+    public function list_monthly(Request $request) {
+        $company_id = Auth::user()->company_id;
+        $periode = Carbon::now()->format('Y-m');
+        if ($request->periode) {
+            $periode = $request->periode;
+        }
+
+        $cash_in = CashIn::where('datetime', 'like', $periode . '%')
+            ->where('company_id', '=', $company_id)->orderBy('datetime')->get();
+        $cash_out = CashIn::where('datetime', 'like', $periode . '%')
+            ->where('company_id', '=', $company_id)->orderBy('datetime')->get();
+
+        $result_data = array();
+        foreach($cash_in AS $item) {
+            $date = Carbon::createFromFormat('Y-m-d H:i:s', $item->datetime)->format('d');
+            $key = array_search($date, array_column($result_data, 'date'));
+            if ($key) {
+                $result_data[$key]->cash_in += (int)$item->fund;
+            } else {
+                 array_push($result_data, array([
+                    'date' => $date,
+                    'cash_in' => (int)$item->fund,
+                    'cash_out' => 0,
+                 ]));
+            }
+        }
+        foreach($cash_out AS $item) {
+            $date = Carbon::createFromFormat('Y-m-d H:i:s', $item->datetime)->format('d');
+            $key = array_search($date, array_column($result_data, 'date'));
+            if ($key) {
+                $result_data[$key]->cash_out += (int)$item->fund;
+            } else {
+                 array_push($result_data, array([
+                    'date' => $date,
+                    'cash_in' => 0,
+                    'cash_out' => (int)$item->fund,
+                 ]));
+            }
+        }
+
+        usort($result_data, function($first, $second) {
+            return $first->date > $second->date;
+        });
+    }
+
+    public function list_daily(Request $request) {
+        $company_id = Auth::user()->company_id;
+        $periode = Carbon::now()->format('Y-m-d');
+        if ($request->periode) {
+            $periode = $request->periode;
+        }
+
+        $cash_in = CashIn::where('datetime', 'like', $periode . '%')
+            ->where('company_id', '=', $company_id)->orderBy('datetime')->get();
+        $cash_out = CashIn::where('datetime', 'like', $periode . '%')
+            ->where('company_id', '=', $company_id)->orderBy('datetime')->get();
+    }
+
+    public function add_data(Request $request) {}
+}
