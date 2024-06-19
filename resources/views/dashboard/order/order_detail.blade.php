98ddd9f07eb60ab7ab2295ce17aeaf59f2620540
yogaprianjaya <yogaprianjaya@gmail.com>
2024-06-20 06:38:17 +0700

    update detail order responsive

--- resources/views/dashboard/order/order_detail.blade.php
+++ resources/views/dashboard/order/order_detail.blade.php
@@ -30,119 +30,127 @@
     </div>
 
     <div class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 sm:p-6 mb-4">
-      <div class="mb-4 space-y-12">
-        <table class="table-fixed divide-y divide-gray-200 w-full">
-          <thead class="divide-y divide-gray-200">
-            <tr class="bg-gray-100">
-              <td colspan="4" class="p-4 text-center text-lg font-bold uppercase text-gray-500"> ID Order
-                {{ $data->id_order }}</td>
-            </tr>
-            <tr>
-              <th class="p-4 text-left text-base font-bold uppercase text-gray-500">Cashier</th>
-              <td class="p-4 text-sm font-normal text-gray-900">
-                {{ $data->cashier_name }}</td>
+      <div class="flex flex-col">
+        <div class="overflow-x-auto">
+          <div class="inline-block min-w-full align-middle">
+            <div class="overflow-hidden shadow">
+              <div class="mb-4 space-y-12">
+                <table class="table-fixed divide-y divide-gray-200 w-full">
+                  <thead class="divide-y divide-gray-200">
+                    <tr class="bg-gray-100">
+                      <td colspan="4" class="p-4 text-center text-lg font-bold uppercase text-gray-500"> ID Order
+                        {{ $data->id_order }}</td>
+                    </tr>
+                    <tr>
+                      <th class="p-4 text-left text-base font-bold uppercase text-gray-500">Cashier</th>
+                      <td class="p-4 text-sm font-normal text-gray-900">
+                        {{ $data->cashier_name }}</td>
 
-              <th class="p-4 text-left text-base font-bold uppercase text-gray-500">Date</th>
-              <td class="p-4 text-sm font-normal text-gray-900">
-                {{ $data->datetime }}</td>
-            </tr>
-            <tr>
-              <th class="p-4 text-left text-base font-bold uppercase text-gray-500">Customer</th>
-              <td class="p-4 text-sm font-normal text-gray-900">
-                {{ $data->customer_name }}</td>
+                      <th class="p-4 text-left text-base font-bold uppercase text-gray-500">Date</th>
+                      <td class="p-4 text-sm font-normal text-gray-900">
+                        {{ $data->datetime }}</td>
+                    </tr>
+                    <tr>
+                      <th class="p-4 text-left text-base font-bold uppercase text-gray-500">Customer</th>
+                      <td class="p-4 text-sm font-normal text-gray-900">
+                        {{ $data->customer_name }}</td>
 
-              <th class="p-4 text-left text-base font-bold uppercase text-gray-500">Order Type</th>
-              <td class="p-4 text-sm font-normal text-gray-900">
-                {{ $data->order_type == 'dine_in' ? 'DINE IN' : 'TAKE AWAY' }}</td>
-            </tr>
-            <tr>
-              <th class="p-4 text-left text-base font-bold uppercase text-gray-500">Remark</th>
-              <td class="p-4 text-sm font-normal text-gray-900" colspan="3">
-                {{ $data->remarks }}</td>
-            </tr>
-            <tr></tr>
-          </thead>
-        </table>
+                      <th class="p-4 text-left text-base font-bold uppercase text-gray-500">Order Type</th>
+                      <td class="p-4 text-sm font-normal text-gray-900">
+                        {{ $data->order_type == 'dine_in' ? 'DINE IN' : 'TAKE AWAY' }}</td>
+                    </tr>
+                    <tr>
+                      <th class="p-4 text-left text-base font-bold uppercase text-gray-500">Remark</th>
+                      <td class="p-4 text-sm font-normal text-gray-900" colspan="3">
+                        {{ $data->remarks }}</td>
+                    </tr>
+                    <tr></tr>
+                  </thead>
+                </table>
 
-        <table class="min-w-full table-fixed divide-y divide-gray-200">
-          <thead class="bg-gray-100">
-            <tr>
-              <th scope="col" class="p-4 text-left text-base font-bold uppercase text-gray-500">
-                Product
-              </th>
-              <th scope="col" class="p-4 text-left text-base font-bold uppercase text-gray-500">
-                Remark
-              </th>
-              <th scope="col" class="p-4 text-left text-base font-bold uppercase text-gray-500">
-                Quantity
-              </th>
-              <th scope="col" class="p-4 text-right text-base font-bold uppercase text-gray-500">
-                Price
-              </th>
-              <th scope="col" class="p-4 text-right text-base font-bold uppercase text-gray-500">
-                Amount
-              </th>
-            </tr>
-          </thead>
-          <tbody class="divide-y divide-gray-200 bg-white">
-            @forelse ($data->items as $item)
-              <tr>
-                <td class="whitespace-nowrap p-4 text-sm font-normal text-gray-500">
-                  <p class="text-sm font-normal text-gray-900">
-                    {{ $item->product->name }}
-                  </p>
-                </td>
-                <td class="whitespace-nowrap p-4 text-sm font-normal text-gray-500">
-                  <p class="text-sm font-normal text-gray-900">
-                    {{ $item->remarks }}
-                  </p>
-                </td>
-                <td class="whitespace-nowrap p-4 text-sm font-normal text-gray-500">
-                  <p class="text-sm font-normal text-gray-900">
-                    {{ $item->quantity }}
-                  </p>
-                </td>
-                <td class="text-right whitespace-nowrap p-4 text-sm font-normal text-gray-500">
-                  <p class="text-sm font-normal text-gray-900">
-                    {{ format_rupiah($item->product->price) }}
-                  </p>
-                </td>
-                <td class="whitespace-nowrap p-4 text-sm font-normal text-gray-500">
-                  <p class="text-sm font-normal text-gray-900 text-end">
-                    {{ format_rupiah($item->amount) }}
-                  </p>
-                </td>
-              </tr>
-            @empty
-              <tr>
-                <td class="text-center text-base font-light p-4" colspan="7">Empty Data</td>
-              </tr>
-            @endforelse
-          </tbody>
-          <tfoot class="bg-gray-100">
-            <tr>
-              <td class="text-start p-2 text-base font-bold uppercase text-gray-500">
-                {{ count($data->items) }} items</td>
-              <td colspan="3" class="text-end p-2 text-base font-bold uppercase text-gray-500">Total
-              </td>
-              <td class="text-end p-2 text-base font-bold uppercase text-gray-500">
-                {{ format_rupiah($data->total_payment) }}</td>
-            </tr>
-            <tr>
-              <td colspan="4" class="text-end p-2 text-base font-bold uppercase text-gray-500">
-                {{ $data->payment_method }}</td>
-              <td class="text-end p-2 text-base font-bold uppercase text-gray-500">
-                {{ format_rupiah($data->payment) }}</td>
-            </tr>
-            <tr>
-              <td colspan="4" class="text-end p-2 text-base font-bold uppercase text-gray-500">
-                Change</td>
-              <td class="text-end p-2 text-base font-bold uppercase text-gray-500">
-                {{ format_rupiah($data->change) }}</td>
-            </tr>
-            </tr>
-          </tfoot>
-        </table>
+                <table class="min-w-full table-fixed divide-y divide-gray-200">
+                  <thead class="bg-gray-100">
+                    <tr>
+                      <th scope="col" class="p-4 text-left text-base font-bold uppercase text-gray-500">
+                        Product
+                      </th>
+                      <th scope="col" class="p-4 text-left text-base font-bold uppercase text-gray-500">
+                        Remark
+                      </th>
+                      <th scope="col" class="p-4 text-left text-base font-bold uppercase text-gray-500">
+                        Quantity
+                      </th>
+                      <th scope="col" class="p-4 text-right text-base font-bold uppercase text-gray-500">
+                        Price
+                      </th>
+                      <th scope="col" class="p-4 text-right text-base font-bold uppercase text-gray-500">
+                        Amount
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody class="divide-y divide-gray-200 bg-white">
+                    @forelse ($data->items as $item)
+                      <tr>
+                        <td class="whitespace-nowrap p-4 text-sm font-normal text-gray-500">
+                          <p class="text-sm font-normal text-gray-900">
+                            {{ $item->product->name }}
+                          </p>
+                        </td>
+                        <td class="whitespace-nowrap p-4 text-sm font-normal text-gray-500">
+                          <p class="text-sm font-normal text-gray-900">
+                            {{ $item->remarks }}
+                          </p>
+                        </td>
+                        <td class="whitespace-nowrap p-4 text-sm font-normal text-gray-500">
+                          <p class="text-sm font-normal text-gray-900">
+                            {{ $item->quantity }}
+                          </p>
+                        </td>
+                        <td class="text-right whitespace-nowrap p-4 text-sm font-normal text-gray-500">
+                          <p class="text-sm font-normal text-gray-900">
+                            {{ format_rupiah($item->product->price) }}
+                          </p>
+                        </td>
+                        <td class="whitespace-nowrap p-4 text-sm font-normal text-gray-500">
+                          <p class="text-sm font-normal text-gray-900 text-end">
+                            {{ format_rupiah($item->amount) }}
+                          </p>
+                        </td>
+                      </tr>
+                    @empty
+                      <tr>
+                        <td class="text-center text-base font-light p-4" colspan="7">Empty Data</td>
+                      </tr>
+                    @endforelse
+                  </tbody>
+                  <tfoot class="bg-gray-100">
+                    <tr>
+                      <td class="text-start p-2 text-base font-bold uppercase text-gray-500">
+                        {{ count($data->items) }} items</td>
+                      <td colspan="3" class="text-end p-2 text-base font-bold uppercase text-gray-500">Total
+                      </td>
+                      <td class="text-end p-2 text-base font-bold uppercase text-gray-500">
+                        {{ format_rupiah($data->total_payment) }}</td>
+                    </tr>
+                    <tr>
+                      <td colspan="4" class="text-end p-2 text-base font-bold uppercase text-gray-500">
+                        {{ $data->payment_method }}</td>
+                      <td class="text-end p-2 text-base font-bold uppercase text-gray-500">
+                        {{ format_rupiah($data->payment) }}</td>
+                    </tr>
+                    <tr>
+                      <td colspan="4" class="text-end p-2 text-base font-bold uppercase text-gray-500">
+                        Change</td>
+                      <td class="text-end p-2 text-base font-bold uppercase text-gray-500">
+                        {{ format_rupiah($data->change) }}</td>
+                    </tr>
+                    </tr>
+                  </tfoot>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
