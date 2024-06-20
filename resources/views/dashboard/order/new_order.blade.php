e08594ebaa02fc53720d047852d1a053312cf418
yogaprianjaya <yogaprianjaya@gmail.com>
2024-06-20 16:05:13 +0700

    update typo

--- resources/views/dashboard/order/new_order.blade.php
+++ resources/views/dashboard/order/new_order.blade.php
@@ -215,7 +215,7 @@
           <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b">
             <button onclick="set_order_to_cart()" data-modal-hide="modal-add-to-cart" type="button"
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Yes,
-              I'm suret</button>
+              I'm sure</button>
             <button data-modal-hide="modal-add-to-cart" type="button"
               class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">No,
               Cancel</button>
