0949473e45e5f46a5c06c153f52d2ae0419a10e9
yogaprianjaya <yogaprianjaya@gmail.com>
2024-06-21 15:15:23 +0700

    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index() {
+        $order = Order::where('company_id', Auth::user()->company_id)
+            ->select()
+            ->where('datetime', 'like', Carbon::now()->toDateString() . '%')
+            ->get();
+        dd($order);
         return view('dashboard.dashboard.index');
     }
 }
--- resources/views/dashboard/dashboard/index.blade.php
+++ resources/views/dashboard/dashboard/index.blade.php
@@ -1,4 +1,59 @@
 @extends('layouts.index')
 
 @section('main')
+<div class="">
+    <div class="mb-4">
+      <nav class="mb-5 flex" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 text-sm font-medium md:space-x-2">
+          <li class="inline-flex items-center">
+            <a href="#"
+              class="inline-flex items-center text-gray-700 hover:text-primary-600">
+              Dashboard
+            </a>
+          </li>
+        </ol>
+      </nav>
+      <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl mb-4">Dashboard</h1>
+    </div>
+  </div>
+
+  @if (Auth::user()->id == 1)
+    <div>
+      <div class="grid grid-cols-3 gap-4 mb-4">
+        <div class="flex justify-center p-3 h-24 rounded-lg bg-green-200 shadow-md border">
+          <div class="flex flex-col justify-center items-center w-3/5">
+            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.6 16.733c.234.269.548.456.895.534a1.4 1.4 0 0 0 1.75-.762c.172-.615-.446-1.287-1.242-1.481-.796-.194-1.41-.861-1.241-1.481a1.4 1.4 0 0 1 1.75-.762c.343.077.654.26.888.524m-1.358 4.017v.617m0-5.939v.725M4 15v4m3-6v6M6 8.5 10.5 5 14 7.5 18 4m0 0h-3.5M18 4v3m2 8a5 5 0 1 1-10 0 5 5 0 0 1 10 0Z"></path>
+            </svg>
+            <p class="text-base text-gray-500">Penjualan Hari Ini</p>
+          </div>
+          <div class="flex justify-center items-center w-2/5">
+            <p class="text-4xl font-bold text-gray-500">11</p>
+          </div>
+        </div>
+        <div class="flex justify-center p-3 h-24 rounded-lg bg-yellow-200 shadow-md border">
+          <div class="flex flex-col justify-center items-center w-3/5">
+            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.6 16.733c.234.269.548.456.895.534a1.4 1.4 0 0 0 1.75-.762c.172-.615-.446-1.287-1.242-1.481-.796-.194-1.41-.861-1.241-1.481a1.4 1.4 0 0 1 1.75-.762c.343.077.654.26.888.524m-1.358 4.017v.617m0-5.939v.725M4 15v4m3-6v6M6 8.5 10.5 5 14 7.5 18 4m0 0h-3.5M18 4v3m2 8a5 5 0 1 1-10 0 5 5 0 0 1 10 0Z"></path>
+            </svg>
+            <p class="text-base text-gray-500">Penjualan Bulan Ini</p>
+          </div>
+          <div class="flex justify-center items-center w-2/5">
+            <p class="text-4xl font-bold text-gray-500">11</p>
+          </div>
+        </div>
+        <div class="flex justify-center p-3 h-24 rounded-lg bg-sky-200 shadow-md border">
+          <div class="flex flex-col justify-center items-center w-3/5">
+            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"></path>
+            </svg>
+            <p class="text-base text-gray-500">Product Terjual Bulan Ini</p>
+          </div>
+          <div class="flex justify-center items-center w-2/5">
+            <p class="text-4xl font-bold text-gray-500">11</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  @endif
 @endsection
