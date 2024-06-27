e12bc9c63119263aa15a2f63fc5ef20691523c7a
yogaprianjaya <yogaprianjaya@gmail.com>
2024-06-27 22:52:18 +0700

    update report

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -505,7 +505,67 @@ class OrderController extends Controller
     }
 
     public function report() {
+        $month = Carbon::now()->format('Y-m');
+
+        $order_month_now = Order::where('company_id', Auth::user()->company_id)
+            ->select(DB::raw("SUM(total_payment) as total_payment"), DB::raw("COUNT(id) as total_order"))
+            ->where('status', 'done')
+            ->where('datetime', 'like', $month . '%')
+            ->first();
+
+        $order_item = OrderItems::where('company_id', Auth::user()->company_id)
+            ->select(DB::raw("SUM(quantity) as quantity"))
+            ->leftJoin('orders', 'orders.id', '=', 'order_items.order_id')
+            ->where('status', 'done')
+            ->where('datetime', 'like', $month . '%')
+            ->first();
         
-        return view('dashboard.order.report');
+        $order_month_chart = Order::where('company_id', Auth::user()->company_id)
+            ->select(DB::raw("COUNT(total_payment) AS count_order"), DB::raw("SUM(total_payment) as total_payment"), DB::raw('Date(datetime) AS date'), DB::raw('AVG(total_payment) AS avg_payment'))
+            ->where('status', 'done')
+            ->where('datetime', 'like', $month . '%')
+            ->groupBy(DB::raw('DATE(datetime)'))
+            ->orderBy(DB::raw('DATE(datetime)'), 'ASC')
+            ->get();
+
+        $result_chart_order_label = array();
+        $result_chart_order_value = array();
+        $result_chart_order_value_avg = array();
+        $result_chart_order_value_count = array();
+        $index_data_chart = 0;
+        for ($i = 1; $i <= (int) Carbon::now()->endOfMonth()->format('d'); $i++) {
+            $days = $month . '-' . ($i < 10 ? '0' . $i : $i);
+            $fund = 0;
+            $avg = 0;
+            $count = 0;
+            if ($index_data_chart < count($order_month_chart) && $order_month_chart[$index_data_chart]->date == $days) {
+                $fund = $order_month_chart[$index_data_chart]->total_payment;
+                $avg = $order_month_chart[$index_data_chart]->avg_payment;
+                $count = $order_month_chart[$index_data_chart]->count_order;
+                $index_data_chart += 1;
+            }
+            array_push($result_chart_order_label, $i < 10 ? '0' . $i : $i);
+            array_push($result_chart_order_value, $fund);
+            array_push($result_chart_order_value_avg, $avg);
+            array_push($result_chart_order_value_count, $count);
+        }
+
+        $order_item_chart = OrderItems::where('company_id', Auth::user()->company_id)
+            ->leftJoin('orders', 'order_items.order_id', '=', 'orders.id')
+            ->select(DB::raw("SUM(quantity) as total_quantity"), DB::raw('Date(created_at) AS date'))
+            ->where('status', 'done')
+            ->where('created_at', 'like', $month . '%')
+            ->groupBy(DB::raw('DATE(created_at)'))
+            ->orderBy(DB::raw('DATE(created_at)'), 'ASC')
+            ->get();
+
+        return view('dashboard.order.report', [
+            'total_order' => $order_month_now,
+            'total_item_order' => $order_item,
+            'result_chart_order_label' => $result_chart_order_label,
+            'result_chart_order_value' => $result_chart_order_value,
+            'result_chart_order_value_avg' => $result_chart_order_value_avg,
+            'result_chart_order_value_count' => $result_chart_order_value_count,
+        ]);
     }
 }
