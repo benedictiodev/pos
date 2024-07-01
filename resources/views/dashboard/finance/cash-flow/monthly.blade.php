dfdd9aa3619ec7c6758b7d81b770a22d10d76c6e
yogaprianjaya <yogaprianjaya@gmail.com>
2024-07-01 11:59:19 +0700

    revert

--- resources/views/dashboard/finance/cash-flow/monthly.blade.php
+++ resources/views/dashboard/finance/cash-flow/monthly.blade.php
@@ -137,7 +137,7 @@
                         </td>
                       </tr>
                     @endif
-                  @else
+                  @elseif ($type_closing_cycle == 'clear' || $type_closing_cycle == 'add_equite')
                     <tr>
                       <td scope="col" colspan="6"
                         class="p-4 text-center text-base font-normal uppercase text-black">
