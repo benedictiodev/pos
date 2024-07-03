6bc14de624d43603143330aa77fff632170d4c7d
yogaprianjaya <yogaprianjaya@gmail.com>
2024-07-03 15:12:31 +0700

    update sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -73,12 +73,14 @@
                   Order History
                 </a>
               </li>
-              <li>
-                <a href="{{ route('dashboard.order.report') }}"
-                  class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 {{ str_contains(Request::route()->getName(), 'dashboard.order.report') ? 'bg-gray-100' : '' }}">
-                  Report
-                </a>
-              </li>
+              @if (Auth::user()->id == 1)
+                <li>
+                  <a href="{{ route('dashboard.order.report') }}"
+                    class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 {{ str_contains(Request::route()->getName(), 'dashboard.order.report') ? 'bg-gray-100' : '' }}">
+                    Report
+                  </a>
+                </li>
+              @endif
             </ul>
           </li>
           {{-- END ORDER --}}
