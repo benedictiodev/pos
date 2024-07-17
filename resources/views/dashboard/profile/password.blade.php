fb0ce536787aef12caed8a6f4ed302f6170f1b2b
yogaprianjaya <yogaprianjaya@gmail.com>
2024-07-17 10:45:58 +0700

    update translate profile

--- resources/views/dashboard/profile/password.blade.php
+++ resources/views/dashboard/profile/password.blade.php
@@ -8,18 +8,18 @@
           <li class="inline-flex items-center">
             <a href="#"
               class="inline-flex items-center text-gray-700 hover:text-primary-600">
-              Dashboard
+              Beranda
             </a>
           </li>
           <li>
             <div class="flex items-center">
               <x-fas-chevron-right class="h-3 w-3 text-gray-400" />
-              <span class="ml-1 text-gray-400 md:ml-2" aria-current="page">Change Password</span>
+              <span class="ml-1 text-gray-400 md:ml-2" aria-current="page">Perbarui Kata Sandi</span>
             </div>
           </li>
         </ol>
       </nav>
-      <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl mb-4">Change Password</h1>
+      <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl mb-4">Perbarui Kata Sandi</h1>
     </div>
 
     @if (session('success'))
@@ -40,27 +40,27 @@
           @csrf
           <div class="space-y-6">
             <div>
-              <label for="old_password" class="mb-2 block text-sm font-medium text-gray-900">Old Password</label>
+              <label for="old_password" class="mb-2 block text-sm font-medium text-gray-900">Kata Sandi Lama</label>
               <input type="password" name="old_password" id="old_password"
                 class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-600 focus:ring-primary-600"
-                placeholder="Old Password" required>
+                placeholder="Kata Sandi Lama" required>
             </div>
             <div>
-              <label for="new_password" class="mb-2 block text-sm font-medium text-gray-900">New Password</label>
+              <label for="new_password" class="mb-2 block text-sm font-medium text-gray-900">Kata Sandi Baru</label>
               <input type="password" name="new_password" id="new_password"
                 class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-600 focus:ring-primary-600"
-                placeholder="New Password" required>
+                placeholder="Kata Sandi Baru" required>
             </div>
             <div>
-              <label for="confirm_password" class="mb-2 block text-sm font-medium text-gray-900">Confirm Password</label>
+              <label for="confirm_password" class="mb-2 block text-sm font-medium text-gray-900">Konfirmasi Kata Sandi</label>
               <input type="password" name="confirm_password" id="confirm_password"
                 class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-600 focus:ring-primary-600"
-                placeholder="Confirm Password" required>
+                placeholder="Konfirmasi Kata Sandi" required>
             </div>
             
             <button type="submit"
               class="w-fit justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300">
-              Submit
+              Kirim
             </button>
           </div>
         </form>
