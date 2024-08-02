26d0da93d753da21d09d7bd305a4d7bdec3d2241
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2024-08-02 15:30:17 +0700

    z-index sidebar hide content by maps

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside id="sidebar"
-  class="fixed top-0 left-0 z-20 flex flex-col flex-shrink-0 w-64 h-full pt-16 font-normal duration-75 lg:flex transition-width hidden"
+  class="fixed top-0 left-0 z-[99999] flex flex-col flex-shrink-0 w-64 h-full pt-16 font-normal duration-75 lg:flex transition-width hidden"
   aria-label="Sidebar">
   <div class="relative flex flex-col flex-1 min-h-0 pt-0 bg-white shadow-lg rounded-br-xl border-r border-gray-200">
     <div class="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto">
