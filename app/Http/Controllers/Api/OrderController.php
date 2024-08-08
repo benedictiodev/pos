35a19f1f2288d243b765faaea80f79b615c328df
yogaprianjaya <yogaprianjaya@gmail.com>
2024-08-08 13:04:51 +0700

    update api

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -193,18 +193,129 @@ class OrderController extends Controller
         }
     }
 
-    public function order_history_edit() {
-        return response()->json([
-            'status' => 200,
-            'message' => 'Fitur not avilable',
-        ], 200);
+    public function order_history_edit($id) {
+        try {
+            $order = Order::where('id', $id)->first();
+            $data_menu = Product::select('products.*', 'category_products.name AS category_name')
+                ->leftJoin('category_products', 'category_products.id', '=', 'products.category_id')
+                ->where('company_id', 1)
+                ->orderBy('category_id')
+                ->orderBy('products.id')
+                ->get();
+
+            $data_fund = Fund::where('company_id', 1)->get();
+
+            $order_item = OrderItems::where('order_id', $id)
+                ->select('order_items.*', 'products.name')
+                ->leftJoin('products', 'products.id', '=', 'order_items.product_id')
+                ->get();
+
+            return view('dashboard.order.order_history_edit', [
+                'list_menu' => $data_menu,
+                'list_fund' => $data_fund,
+                'order_item' => $order_item,
+                'order' => $order
+            ]);
+        } catch (Throwable $error) {
+            return response()->json([
+                'status' => 500,
+                'message' => $error->getMessage(),
+            ], 500); 
+        }
     }
 
-    public function order_history_update() {
-        return response()->json([
-            'status' => 200,
-            'message' => 'Fitur not avilable',
-        ], 200);
+    public function order_history_update(Request $request, $id) {
+        try {
+            DB::beginTransaction();
+            $total_payment = (int) str_replace('.', '', $request['confirm_order-total_payment']);
+            $payment = (int) str_replace('.', '', $request['confirm_order-payment']);
+            $change = (int) str_replace('.', '', $request['confirm_order-change']);
+            $order_add = json_decode($request['confirm_order-order_add']);
+            $order_update = json_decode($request['confirm_order-order_update']);
+            $order_delete = json_decode($request['confirm_order-order_delete']);
+
+            $update_order = Order::where('id', $id)
+                ->where('company_id', 1)
+                ->update([
+                    'customer_name' => $request['confirm_order-customer_name'],
+                    'total_payment' => $total_payment,
+                    'payment' => $payment,
+                    'change' => $change,
+                    'payment_method' => $request['confirm_order-payment_method'],
+                    'order_type' => $request['confirm_order-order_type'],
+                    'remarks' => $request['confirm_order-remarks'],
+                ]);
+
+            foreach ($order_add as $item) {
+                OrderItems::create([
+                    'order_id' => $id,
+                    'product_id' => $item->product_id,
+                    'price' => $item->product_price,
+                    'quantity' => $item->qty,
+                    'amount' => $item->product_price * $item->qty,
+                    'remarks' => $item->remarks,
+                ]);
+            }
+
+            foreach ($order_delete as $item) {
+                OrderItems::where('id', $item)->delete();
+            }
+
+            foreach ($order_update as $item) {
+                OrderItems::where('id', $item->id)
+                    ->update([
+                        'order_id' => $id,
+                        'product_id' => $item->product_id,
+                        'price' => $item->product_price,
+                        'quantity' => $item->qty,
+                        'amount' => $item->product_price * $item->qty,
+                        'remarks' => $item->remarks,
+                    ]);
+            }
+
+            $old_data_cash = CashIn::where('company_id', 1)
+                ->where('order_id', $id)->first();
+
+            CashIn::where('id', $old_data_cash->id)->update([
+                'fund' => $total_payment,
+                'type' => $request['confirm_order-payment_method'],
+            ]);
+
+            $closing_cyle = ClosingCycle::where("company_id", 1)
+                ->where("periode", Carbon::parse($old_data_cash->datetime)->format('Y-m'))
+                ->first();
+
+            if ($closing_cyle) {
+                $fund_old = Fund::where("company_id", 1)
+                    ->where("type", $old_data_cash->type)->first();
+                $fund_old->update(["fund" => $fund_old->fund - $old_data_cash->fund]);
+
+                $fund_new = Fund::where("company_id", 1)
+                    ->where("type", $request["confirm_order-payment_method"])->first();
+                $fund_new->update(["fund" => $fund_new->fund + $total_payment]);
+            }
+
+            $cash_monthly = CashMonthly::where("company_id", 1)
+                ->where("datetime", Carbon::parse($old_data_cash->datetime)->toDateString())->first();
+
+            CashMonthly::where("id", $cash_monthly->id)->update([
+                "kredit" => (int) $cash_monthly->kredit - $old_data_cash->fund + $total_payment,
+                "amount" => (int) $cash_monthly->amount - $old_data_cash->fund + $total_payment,
+                "total_amount" => $closing_cyle ? (int) $cash_monthly->total_amount - $old_data_cash->fund + $total_payment : 0,
+            ]);
+
+            DB::commit();
+            return response()->json([
+                'status' => 200,
+                'message' => 'Order berhasil diperbaharui',
+            ], 200);
+        } catch (Throwable $error) {
+            DB::rollBack();
+            return response()->json([
+                'status' => 500,
+                'message' => $error->getMessage(),
+            ], 500); 
+        }
     }
 
     public function order_detail($id) {
@@ -224,11 +335,26 @@ class OrderController extends Controller
         }
     }
 
-    public function delete_order() {
-        return response()->json([
-            'status' => 200,
-            'message' => 'Fitur not avilable',
-        ], 200);
+    public function delete_order($id) {
+        try {
+            DB::beginTransaction();
+            $data =  Order::findOrFail($id);
+            OrderItems::where('order_id', $id)->delete();
+            Order::where('id', $id)->delete();
+
+            DB::commit();
+            return response()->json([
+                'status' => 200,
+                'message' => 'Penghapusan data berhasil',
+                'data' => $data,
+            ], 200);
+        } catch (Throwable $error) {
+            DB::rollBack();
+            return response()->json([
+                'status' => 500,
+                'message' => $error->getMessage(),
+            ], 500); 
+        }
     }
 
     public function edit_order($id) {
